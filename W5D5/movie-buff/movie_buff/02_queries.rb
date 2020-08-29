4206b20ee7d340abedcb39f5d749afe6d1fcc9dd
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-08-28 18:12:48 -0700

    Added W5D5

---
+++ W5D5/movie-buff/movie_buff/02_queries.rb
@@ -0,0 +1,52 @@
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between
+  # 3 and 5 (inclusive).
+  # Show the id, title, year, and score.
+
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989)
+    .where(score: 3..5)
+    # .where('yr BETWEEN 1980 AND 1989')
+    # .where('score BETWEEN 3 AND 5')
+end
+
+def bad_years
+  # List the years in which a movie with a rating above 8 was not released.
+
+  # Movie
+  #   .select(:yr)
+  #   .where()
+
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+  
+  Movie
+    .select("actors.id, actors.name")
+    .joins(:actors)
+    .where(movies: {title: title})
+    .order("castings.ord ASC")
+
+    # .order(castings: {ord: :asc})   # why does this not work?
+    # .joins(:castings)
+    # .where("movies.title = #{title}")
+
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared
+  # as the starring actor.
+  # Show the movie id and title and director's name.
+
+  # Note: Directors appear in the 'actors' table.
+
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles.
+  # Show each actor's id, name and number of supporting roles.
+
+end
